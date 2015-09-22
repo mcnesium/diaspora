8c025a3d1d2ac4cb3954f8642327f4e2e7583ab6
mcnesium <git@mcnesium.com>
2015-09-22 17:43:54 +0200

    spec test (b0rk)

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,12 +2,12 @@ class Event < ActiveRecord::Base
   # include Diaspora::Federated::Base
   # include Diaspora::Guid
 
-#  belongs_to :status_message
+  # belongs_to :status_message
 
-#  xml_attr :title
-#  xml_attr :location
-#  xml_attr :start
-#  xml_attr :end
+  # xml_attr :title
+  # xml_attr :location
+  # xml_attr :start
+  # xml_attr :end
 
   # forward some requests to status message
   delegate :author, :author_id, :diaspora_handle, :public?, :subscribers, to: :status_message
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,10 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Event, :type => :model do
+
+  it 'is a valid object' do
+    expect(event_1.valid?).to be true
+    # TODO RAILS_ENV="test" bin/rake db:create db:schema:load fails
+  end
+
 end
