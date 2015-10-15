a92e64473d1002b84dac883bf622236f232ae25c
mcnesium <git@mcnesium.com>
2015-10-15 22:06:34 +0200

    db foo

--- db/migrate/20151002103814_add_index_to_event_participations.rb
+++
@@ -1,8 +0,0 @@
-class AddIndexToEventParticipations < ActiveRecord::Migration
-  def change
-    rename_column :event_participations, :person, :person_id
-
-    add_index :event_participations, [:event_id, :person_id], unique: true,
-                :length => { :event_id => 32, :person_id => 32 }
-  end
-end
--- db/migrate/20151010162807_create_events.rb
+++ db/migrate/20151010162807_create_events.rb
@@ -10,7 +10,7 @@ class CreateEvents < ActiveRecord::Migration
     end
 
     create_table :event_participations do |t|
-      t.string :person
+      t.string :person_id
       t.string :event
       t.string :invited_by
       t.boolean :attending
@@ -19,5 +19,7 @@ class CreateEvents < ActiveRecord::Migration
       t.timestamps null: false
     end
 
+    add_index :event_participations, [:event_id, :person_id], unique: true,
+                :length => { :event_id => 32, :person_id => 32 }
   end
 end
