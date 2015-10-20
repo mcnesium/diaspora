a0abb2e70053f30722d6475f28e0401ce0740e09
mcnesium <git@mcnesium.com>
2015-10-20 14:34:23 +0200

    remote event owner? bork

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,22 +1,11 @@
 class Event < ActiveRecord::Base
   include Diaspora::Federated::Base
   include Diaspora::Guid
-  # include Diaspora::Shareable
-
-  # belongs_to :status_message
 
   has_many :event_participations
 
-  # xml_attr :guid
   xml_attr :title
   xml_attr :start
-  # xml_attr :end
-  # xml_attr :location
-  # xml_attr :image
-
-  # forward some requests to status message
-  # delegate :author, :author_id, :diaspora_handle, :public?, :subscribers, to: :status_message
-  # TODO  Location
 
   validates :title, length: { minimum: 1, maximum: 255 }
   validates :start, presence: true
@@ -28,6 +17,9 @@ class Event < ActiveRecord::Base
     self.owner.diaspora_handle
   end
   def owner
-    current_user.person
+    # byebug
+    participation = EventParticipation.find_by(event: self);
+    person = Person.find_by(participation.person_id)
+    return person
   end
 end
