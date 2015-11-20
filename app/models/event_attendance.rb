80c479553ec257c2d54f4c07e2157244ca1f0d86
mcnesium <git@mcnesium.com>
2015-11-20 16:51:33 +0100

    federate attendance

--- app/models/event_attendance.rb
+++ app/models/event_attendance.rb
@@ -9,4 +9,29 @@ class EventAttendance < ActiveRecord::Base
 
   validates_uniqueness_of :event, scope: [:attendee]
 
+  xml_name :event_attendance
+  xml_attr :event_guid
+  xml_attr :attendee_guid
+
+  # RuntimeError (You must override subscribers in order to enable federation on this model)
+  def subscribers(user)
+    user.contact_people
+  end
+
+  def event_guid
+    self.event.guid
+  end
+
+  def event_guid= (guid)
+    self.event = Event.find_by_guid(guid)
+  end
+
+  def attendee_guid
+    self.attendee.guid
+  end
+
+  def attendee_guid= (guid)
+    self.attendee = Person.find_by_guid(guid)
+  end
+
 end
