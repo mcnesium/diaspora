6666a693b3b2722973348cd53c52404867240c27
mcnesium <git@mcnesium.com>
2015-11-21 18:00:20 +0100

    first editors

---
+++ app/models/event_editor.rb
@@ -0,0 +1,51 @@
+class EventEditor < ActiveRecord::Base
+  include Diaspora::Federated::Base
+
+  belongs_to :event
+  validates :event, presence: true
+
+  belongs_to :editor, :class_name => 'Person'
+  validates :editor, presence: true
+
+  validates_uniqueness_of :event, scope: [:editor]
+
+  xml_name :event_editor
+  xml_attr :event_guid
+  xml_attr :editor_guid
+  xml_attr :diaspora_handle
+
+  # RuntimeError (You must override subscribers in order to enable federation on this model)
+  def subscribers(user)
+    user.contact_people
+  end
+
+  def event_guid
+    self.event.guid
+  end
+
+  def event_guid= (guid)
+    self.event = Event.find_by_guid(guid)
+  end
+
+  def editor_guid
+    self.editor.guid
+  end
+
+  def editor_guid= (guid)
+    self.editor = Person.find_by_guid(guid)
+  end
+
+  def diaspora_handle
+    self.editor.diaspora_handle
+  end
+
+  def diaspora_handle= (handle)
+    self.editor = Person.find_by_diaspora_handle(handle)
+  end
+
+  # RuntimeError (You must override receive in order to enable federation on this model):
+  def receive(u, p)
+    self.save
+  end
+
+end
--- db/migrate/20151117141231_create_events.rb
+++ db/migrate/20151117141231_create_events.rb
@@ -24,14 +24,13 @@ class CreateEvents < ActiveRecord::Migration
     end
     add_index :event_invitations, [:invitee_id, :event_id], unique: true
 
-    # create_table :event_roles do |t|
-    #   t.belongs_to :person, :null => false
-    #   t.belongs_to :event, :null => false
-    #   t.integer :role, default: 0
-    #   t.timestamps null: false
-    # end
-    # add_index :event_roles, [:person_id, :event_id], unique: true
-    #
+    create_table :event_editors do |t|
+      t.belongs_to :editor, :null => false
+      t.belongs_to :event, :null => false
+      t.timestamps null: false
+    end
+    add_index :event_editors, [:editor_id, :event_id], unique: true
+
   end
 
   def down
@@ -40,8 +39,8 @@ class CreateEvents < ActiveRecord::Migration
     remove_index :event_attendances, [:attendee_id, :event_id]
     drop_table :event_invitations
     remove_index :event_invitations, [:invitee_id, :event_id]
-    # drop_table :event_roles
-    # remove_index :event_roles, [:person_id, :event_id]
+    drop_table :event_editors
+    remove_index :event_editors, [:editor_id, :event_id]
   end
 
 end
