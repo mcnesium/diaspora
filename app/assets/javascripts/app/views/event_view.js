0c2d8f74b53ef3f0c610b2c9701923d8718b280e
mcnesium <git@mcnesium.com>
2015-12-14 18:18:38 +0100

    add events

--- app/assets/javascripts/app/views/event_view.js
+++ app/assets/javascripts/app/views/event_view.js
@@ -1,3 +1,40 @@
+
+var updateEventsList = function() {
+
+  $.getJSON( "/events", function( data ) {
+
+    var items = [];
+    $.each( data, function( key, val ) {
+      var title = val.event.title;
+      var id = val.event.id;
+      items.push( "<li id='" + id + "'>" + title + "</li>" );
+    });
+
+    $( "#known-events").remove();
+    $( "<ul/>", {
+      "id": "known-events",
+      html: items.join( "" )
+    }).appendTo( "#list" );
+  });
+
+}
+
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+  $('a#create').on("click",function( event ){
+    event.preventDefault;
+
+    var title = $("input#title").val();
+
+    $.post( "events", { title: title } )
+      .done(function(data){
+        console.log(data);
+        updateEventsList();
+      })
+  });
+
+
+  console.log('ready');
+  updateEventsList();
+
 });
